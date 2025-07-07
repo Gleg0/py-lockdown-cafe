d2689f28d8c677f3cc1c1f1217e9f6d5b49eb5ba
Glego <glego200323@gmail.com>
2025-07-07 22:02:13 +0300

    Solution

---
+++ app/cafe.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from app.errors import (NotVaccinatedError,
+                        NotWearingMaskError,
+                        OutdatedVaccineError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("Where is vaccine?")
+        elif visitor["vaccine"]["expiration_date"] < datetime.today().date():
+            raise OutdatedVaccineError("When do you get vaccine?")
+        elif not visitor["wearing_a_mask"]:
+            raise NotWearingMaskError("Where is your mask?")
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.cafe import Cafe
+from app.errors import NotWearingMaskError, VaccineError
+
+
+def go_to_cafe(friends: list, cafe: Cafe) -> str:
+    count_not_wearing_mask = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except VaccineError:
+            return "All friends should be vaccinated"
+        except NotWearingMaskError:
+            count_not_wearing_mask += 1
+
+    if count_not_wearing_mask != 0:
+        return f"Friends should buy {count_not_wearing_mask} masks"
+    return f"Friends can go to {cafe.name}"
