9bbc616519867de101649c481eac4702ef562816
Glego <glego200323@gmail.com>
2025-07-08 11:25:09 +0300

    Solution fix flake8

--- app/cafe.py
+++ app/cafe.py
@@ -14,7 +14,9 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Where is vaccine?")
-        elif "expiration_date" not in visitor["vaccine"] or visitor["vaccine"]["expiration_date"] < datetime.today().date():
+        elif ("expiration_date" not in visitor["vaccine"]
+              or visitor["vaccine"]["expiration_date"]
+              < datetime.today().date()):
             raise OutdatedVaccineError("When do you get vaccine?")
         elif not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("Where is your mask?")
